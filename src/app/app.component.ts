9c3d58f8152696e5c0e038c10f6343c1833c805a
Kalinin Maksim <kalinynmv@gamail.com>
2019-01-17 17:57:33 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent {
   public allPointPlayer: string = `player points: `;
   public dealerCarts: string = `dealer cards: `;
   public allPointDealer: string = `dealer points: `;
-  public result: string = `Let's play, a piece of meat?`
-  public togle: boolean = false;
+  public result: string = `Let's play, a piece of meat?`;
+  public togle: boolean = false; //false --> none // true --> inlaine
 
 
   private _deck: number[] = [6, 7, 8, 9, 10, 2, 3, 4, 11,
@@ -24,10 +24,7 @@ export class AppComponent {
   6, 7, 8, 9, 10, 2, 3, 4, 11,
   6, 7, 8, 9, 10, 2, 3, 4, 11];
 
-
-  public constructor(){
-    
-  }
+  
   public startGame(): void {
     this.newDeck();
     this.resetResult();
@@ -49,10 +46,14 @@ export class AppComponent {
     this.handDealer = [];
   
     // document.querySelector('.result').innerHTML = 'Let&#39;s play, a piece of meat?';
+    this.result = `Let's play, a piece of meat?`;
     // document.querySelector('.all-point-dealer').innerHTML = 'dealer points: ';
+    this.allPointDealer = 'dealer points: ';
     // document.querySelector('.all-point-player').innerHTML = 'player points: ';
-  
+    this.allPointPlayer = 'player points: ';
     // document.querySelector('.control-btn').style.display = 'inline';
+    this.togle = true;
+    
   }
 
   public getHandSum(hand): number { //сумма номинала в руке
@@ -65,11 +66,11 @@ export class AppComponent {
     return sum;
   }
 
-  public takeCard() { // взять еще и перебор (игрок и дилер)
+  public takeCard(): void { // взять еще и перебор (игрок и дилер)
     this.handPlayer.push(this.myDeck.pop());
   
-    // document.querySelector('.player-carts').innerHTML = 'player cards: ' + handPlayer;
-    // document.querySelector('.all-point-player').innerHTML = 'player points: ' + getHandSum(handPlayer);
+    this.playerCarts = 'player cards: ' + this.handPlayer;
+    this.allPointPlayer = 'player points: ' + this.getHandSum(this.handPlayer);
   
     if(this.getHandSum(this.handDealer) <= 15) {
       this.takeDealer();
@@ -78,14 +79,16 @@ export class AppComponent {
     }
   
     if(this.getHandSum(this.handPlayer) == 21) {
-      // document.querySelector('.result').innerHTML = ' You Win!!! ';
+     
+      this.result = ' You Win!!! ';
       this.showResult();
   
       return;
     }
   
     if(this.getHandSum(this.handPlayer) > 21) {
-      // document.querySelector('.result').innerHTML = ' You Bust!!! ';
+     debugger;
+      this.result = ' You Bust!!! ';
       this.showResult();
   
       return;
@@ -94,10 +97,12 @@ export class AppComponent {
 
   public takeDealer(): void {
     this.handDealer.push(this.myDeck.pop());
-    // document.querySelector('.dealer-carts').innerHTML = 'dealer cards: ' + handDealer[0];
+   
+    this.dealerCarts = 'dealer cards: ' + this.handDealer[0];
   
     if(this.getHandSum(this.handDealer) > 21) {
-      // document.querySelector('.result').innerHTML = ' Bust dealer!!! Dammit! ';
+      
+      this.result = ' Bust dealer!!! Dammit! ';
       this.showResult();
   
       return;
@@ -105,11 +110,10 @@ export class AppComponent {
   }
 
   public showResult(): void {
-    // document.querySelector('.all-point-dealer').innerHTML = 'dealer points: ' + getHandSum(handDealer);
+  
     this.allPointDealer = 'dealer points: ' + this.getHandSum(this.handDealer);
-    // document.querySelector('.dealer-carts').innerHTML = 'dealer cards: ' + handDealer;
     this.dealerCarts = 'dealer cards: ' + this.handDealer;
-    // document.querySelector('.control-btn').style.display = 'none';
+  
     this.togle = false;
   }
 
@@ -117,21 +121,21 @@ export class AppComponent {
     this.showResult();
   
     if(this.getHandSum(this.handPlayer) == this.getHandSum(this.handDealer)) {
-      // document.querySelector('.result').innerHTML = ' Nobody ';
+    
       this.result = ' Nobody ';
   
       return;
     }
   
     if(this.getHandSum(this.handPlayer) == 21 || this.getHandSum(this.handPlayer) > this.getHandSum(this.handDealer)) {
-      // document.querySelector('.result').innerHTML = ' You Win!!! ';
+    
       this.result = ' You Win!!! ';
   
       return;
     }
   
     if(this.getHandSum(this.handPlayer) < 21 || this.getHandSum(this.handDealer) > this.getHandSum(this.handPlayer)) {
-      // document.querySelector('.result').innerHTML = ' You lose, maybe<br>another time, darling :)) ';
+
       this.result = 'You lose, maybe<br>another time, darling :))';
   
       return;
