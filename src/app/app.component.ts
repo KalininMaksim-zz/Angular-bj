f8801ecd788e8352896e82ec4d873ffde479c85d
Kalinin Maksim <kalinynmv@gamail.com>
2019-01-18 15:01:13 +0200

    angular-blac-jack-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public title: string = 'angular-bj';
-  public handPlayer: number[] = [];
-  public handDealer: number[] = [];
-  public myDeck: number[] = [];
+
   public playerCarts: string = `player carts: `;
   public allPointPlayer: string = `player points: `;
   public dealerCarts: string = `dealer cards: `;
   public allPointDealer: string = `dealer points: `;
   public result: string = `Let's play, a piece of meat?`;
-  public togle: boolean = false; // false --> none // true --> inlaine
+  public toggle: boolean = false; // false --> none // true --> inline
 
-
+  private _handPlayer: number[] = [];
+  private _handDealer: number[] = [];
+  private _myDeck: number[] = [];
   private _deck: number[] = [6, 7, 8, 9, 10, 2, 3, 4, 11,
-  6, 7, 8, 9, 10, 2, 3, 4, 11,
-  6, 7, 8, 9, 10, 2, 3, 4, 11,
-  6, 7, 8, 9, 10, 2, 3, 4, 11];
-  private constWin: number = 21;
-  private constCheck: number = 15;
+    6, 7, 8, 9, 10, 2, 3, 4, 11,
+    6, 7, 8, 9, 10, 2, 3, 4, 11,
+    6, 7, 8, 9, 10, 2, 3, 4, 11];
+  private readonly CHECK_THE_VALUE_WIN: number = 21;
+  private readonly CHECKING_DEALER_ACTIONS: number = 15;
 
 
   public startGame(): void {
@@ -34,44 +33,46 @@ export class AppComponent {
   }
 
   public takeCard(): void { // взять еще и перебор (игрок и дилер)
-    this.handPlayer.push(this.myDeck.pop());
-    this.playerCarts = 'player cards: ' + this.handPlayer;
-    this.allPointPlayer = 'player points: ' + this._getHandSum(this.handPlayer);
+    this._handPlayer.push(this._myDeck.pop());
+    this.playerCarts = 'player cards: ' + this._handPlayer;
+    this.allPointPlayer = 'player points: ' + this._getHandSum(this._handPlayer);
 
-    if (this._getHandSum(this.handPlayer) === this.constWin ) {
-      this.result = ' You Win!!! ';
-      this._showResult();
+    if (this._getHandSum(this._handPlayer) === this.CHECK_THE_VALUE_WIN) {
+      this._showResult('You Win!!!');
+
       return;
     }
 
-    if (this._getHandSum(this.handPlayer) > this.constWin) {
-      this.result = ' You Bust!!! ';
-      this._showResult();
+    if (this._getHandSum(this._handPlayer) > this.CHECK_THE_VALUE_WIN) {
+      this._showResult('You Bust!!!');
+
       return;
     }
 
-    if (this._getHandSum(this.handDealer) <= this.constCheck) {
+    if (this._getHandSum(this._handDealer) <= this.CHECKING_DEALER_ACTIONS) {
       this._takeDealer();
+
       return;
     }
   }
 
   public compareCard(): void {
-    this._showResult();
 
-    if (this._getHandSum(this.handPlayer) === this._getHandSum(this.handDealer)) {
-      this.result = ' Nobody ';
+    if (this._getHandSum(this._handPlayer) === this._getHandSum(this._handDealer)) {
+      this._showResult('Nobody');
+
       return;
     }
 
-    if (this._getHandSum(this.handPlayer) === this.constWin || this._getHandSum(this.handPlayer) > this._getHandSum(this.handDealer)) {
-      this.result = ' You Win!!! ';
+    if (this._getHandSum(this._handPlayer) > this._getHandSum(this._handDealer)) {
+      this._showResult('You Win!!!');
+
       return;
     }
 
-    if (this._getHandSum(this.handPlayer) < this.constWin || this._getHandSum(this.handDealer) > this._getHandSum(this.handPlayer)) {
+    if (this._getHandSum(this._handDealer) > this._getHandSum(this._handPlayer)) {
+      this._showResult('You lose, maybe another time :)');
 
-      this.result = `You lose, maybe another time :)`;
       return;
     }
   }
@@ -79,21 +80,21 @@ export class AppComponent {
 
   private _newDeck(): void {
 
-    if (this.myDeck.length !== 0) {
-      this.myDeck = [...this.myDeck, ...this.handDealer, ...this.handPlayer];
-      this.myDeck.sort(() => .5 - Math.random());
+    if (this._myDeck.length !== 0) {
+      this._myDeck = [...this._myDeck, ...this._handDealer, ...this._handPlayer];
+      this._myDeck.sort(() => .5 - Math.random());
     } else {
-      this.myDeck = this._deck.sort(() => .5 - Math.random());
+      this._myDeck = this._deck.sort(() => .5 - Math.random());
     }
   }
 
   private _resetResult(): void {
-    this.handPlayer = [];
-    this.handDealer = [];
+    this._handPlayer = [];
+    this._handDealer = [];
     this.result = `Let's play, a piece of meat?`;
     this.allPointDealer = 'dealer points: ';
     this.allPointPlayer = 'player points: ';
-    this.togle = true;
+    this.toggle = true;
   }
 
   private _getHandSum(hand: number[]): number { // сумма номинала в руке
@@ -101,22 +102,24 @@ export class AppComponent {
     for (let i: number = 0; hand.length > i; i++) {
       sum += hand[i];
     }
+
     return sum;
   }
 
   private _takeDealer(): void {
-    this.handDealer.push(this.myDeck.pop());
-    this.dealerCarts = 'dealer cards: ' + this.handDealer[0];
-    if (this._getHandSum(this.handDealer) > this.constWin) {
-      this.result = ' Bust dealer!!! Dammit! ';
-      this._showResult();
+    this._handDealer.push(this._myDeck.pop());
+    this.dealerCarts = 'dealer cards: ' + this._handDealer[0];
+    if (this._getHandSum(this._handDealer) > this.CHECK_THE_VALUE_WIN) {
+      this._showResult(' Bust dealer!!! Dammit! ');
       return;
     }
   }
 
-  private _showResult(): void {
-    this.allPointDealer = 'dealer points: ' + this._getHandSum(this.handDealer);
-    this.dealerCarts = 'dealer cards: ' + this.handDealer;
-    this.togle = false;
+  private _showResult(message: string): void {
+    this.result = message;
+    this.allPointDealer = 'dealer points: ' + this._getHandSum(this._handDealer);
+    this.dealerCarts = 'dealer cards: ' + this._handDealer;
+    this.toggle = false;
+
   }
 }
