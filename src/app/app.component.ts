ac44d6c4f5e7421bf409533d1ee7995f41092915
Kalinin Maksim <kalinynmv@gamail.com>
2019-01-24 12:25:44 +0200

    bag fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ export class AppComponent {
   public handPlayer: number[] = [];
   public handDealer: number[] = [];
   public result: string = `Let's play, a piece of meat?`;
-  public itsEnitStayte: boolean = false; // false --> hide // true --> show
+  public itsEnidState: boolean = false; // false --> hide // true --> show
   public sumDealerCards: number = 0;
   public sumPlayerCards: number = 0;
 
 
   private readonly CHECK_THE_VALUE_WIN: number = 21;
-  private readonly CHECKING_DEALER_ACTIONS: number = 15;
+  private readonly MAX_DEALER_SCORE: number = 15;
 
   private _myDeck: number[] = [];
   private _deck: number[] = [6, 7, 8, 9, 10, 2, 3, 4, 11,
@@ -29,9 +29,10 @@ export class AppComponent {
     this._newDeck();
     this._resetResult();
     this.takeCard();
-    this.itsEnitStayte = true;
+    this.itsEnidState = true;
 
   }
+
   public getHandSum(hand: number[]): number { // amount of nominal in hand
     let sum: number = 0;
     hand.forEach(( item: number ) => sum += item);
@@ -54,7 +55,7 @@ export class AppComponent {
       return;
     }
 
-    if (this.sumDealerCards <= this.CHECKING_DEALER_ACTIONS) {
+    if (this.sumDealerCards <= this.MAX_DEALER_SCORE) {
       this._takeDealer();
 
       return;
@@ -86,11 +87,15 @@ export class AppComponent {
     if (this._myDeck.length !== 0) {
       this._myDeck = [...this._myDeck, ...this.handDealer, ...this.handPlayer];
       this._myDeck.sort(() => .5 - Math.random());
+
+      return;
     }
     this._myDeck = this._deck.sort(() => .5 - Math.random());
   }
 
   private _resetResult(): void {
+    this.sumDealerCards = 0;
+    this.sumPlayerCards = 0;
     this.handPlayer = [];
     this.handDealer = [];
     this.result = `Let's play, a piece of meat?`;
@@ -99,7 +104,6 @@ export class AppComponent {
   private _takeDealer(): void {
     this.handDealer.push(this._myDeck.pop());
     this.sumDealerCards = this.getHandSum(this.handDealer);
-
     if (this.sumDealerCards > this.CHECK_THE_VALUE_WIN) {
       this._showResult(' Bust dealer!!! Dammit! ');
 
@@ -109,6 +113,6 @@ export class AppComponent {
 
   private _showResult(message: string): void {
     this.result = message;
-    this.itsEnitStayte = false;
+    this.itsEnidState = false;
   }
 }
